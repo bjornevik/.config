975eb5376ef3024f6cd97ac37f3960313997aa8d
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-03-25 09:20:02 +0100

    fix(debug): add nvim-nio deps for nvim-dap
    .. I don't even know why I have nvim-dap, I don't use it. (Mainly
    because I haven't been able to get it to work 🙈)

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -2,10 +2,9 @@ return {
   "mfussenegger/nvim-dap",
   dependencies = {
     "rcarriga/nvim-dap-ui",
-
+    "nvim-neotest/nvim-nio",
     "williamboman/mason.nvim",
     "jay-babu/mason-nvim-dap.nvim",
-
     "leoluz/nvim-dap-go",
   },
   config = function()
