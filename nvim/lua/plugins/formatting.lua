77d8ee4e7a7563b34aa56bc04ebfc9288154741b
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-11-08 12:52:00 +0100

    fix(nvim): conform.nvim nested {} syntax
    deprecated[conform]: The nested {} syntax to run the first formatter has
    been replaced by the stop_after_first option (see :help conform.format).
    Support for the old syntax will be dropped on 2025-01-01.

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -4,30 +4,31 @@ return {
     lazy = false,
     config = function()
       local conform = require "conform"
-      conform.setup({
+      conform.setup {
         format_on_save = {
           timeout_ms = 500,
           lsp_fallback = true,
         },
         formatters_by_ft = {
-          astro = { { "prettierd", "prettier" } },
-          css = { { "prettierd", "prettier" } },
+          astro = { "prettierd", "prettier" },
+          css = { "prettierd", "prettier" },
           go = { "gofmt" },
-          html = { { "prettierd", "prettier" } },
-          javascript = { { "prettierd", "prettier" } },
+          html = { "prettierd", "prettier" },
+          javascript = { "prettierd", "prettier" },
           json = { "jq" },
           lua = { "stylua" },
-          markdown = { { "prettierd", "prettier" } },
+          markdown = { "prettierd", "prettier" },
           ocaml = { "ocamlformat " },
           rust = { "rustfmt" },
-          typescript = { { "prettierd", "prettier" } },
-          vue = { { "prettierd", "prettier" } },
+          typescript = { "prettierd", "prettier" },
+          vue = { "prettierd", "prettier" },
         },
-      })
+      }
 
       -- <leader>nf cause I used to use (n)eo(f)ormat
-      vim.keymap.set("n", "<leader>nf", function() conform.format({ async = true, lsp_fallback = true }) end,
-        { desc = "Conform: format" })
+      vim.keymap.set("n", "<leader>nf", function()
+        conform.format { async = true, lsp_fallback = true, stop_after_first = true }
+      end, { desc = "Conform: format" })
     end,
   },
 }
