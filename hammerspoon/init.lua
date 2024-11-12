088e815711634348d42cf736103ec9569e1fc5dd
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-11-12 09:07:44 +0100

    fix(hammerspoon): add apple (m)usic and (p)odcasts
    Got an iPhone -> trying apple music with free trial

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -19,7 +19,8 @@ hs.hotkey.bind(hyper, "w", open_app("Firefox")) -- (w)eb
 hs.hotkey.bind(hyper, "o", open_app("Obsidian")) -- (o)bsidian
 hs.hotkey.bind(hyper, "i", open_app("Bruno")) -- formerly (i)nsomnia
 hs.hotkey.bind(hyper, "t", open_app("Kitty")) -- (t)erminal
-hs.hotkey.bind(hyper, "m", open_app("Youtube Music")) -- (m)usic
+hs.hotkey.bind(hyper, "m", open_app("Music")) -- apple (m)usic
+hs.hotkey.bind(hyper, "p", open_app("Podcasts")) -- apple (p)odcas
 hs.hotkey.bind(hyper, "f", open_app("Figma")) -- (f)igma
 
 hs.hotkey.bind(hyper, "q", open_app("Simulator")) -- q cause unused
