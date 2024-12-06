a3b07e740d1cddae799e9c0da203fde1ccbd38db
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-06 15:02:46 +0100

    chore(neoneovim): cleanup colorscheme.lua and use gruvbox-baby

--- neoneovim/lua/plugins/colorscheme.lua
+++ neoneovim/lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
 	{
 		"folke/tokyonight.nvim",
-		enabled = true,
+		enabled = false,
+		lazy = false,
+		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("tokyonight")
+			vim.cmd.colorscheme "tokyonight"
 		end,
 	},
+	{
+		"luisiacc/gruvbox-baby",
+		enabled = true,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_baby_use_original_palette = true
+			vim.cmd.colorscheme "gruvbox-baby"
+		end
+	}
 }
