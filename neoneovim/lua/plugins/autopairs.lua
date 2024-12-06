660c1d4967c148befdd118b6b1f2fbd90eae4429
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-06 15:25:22 +0100

    feat(neoneovim): autopairs

---
+++ neoneovim/lua/plugins/autopairs.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"windwp/nvim-autopairs",
+		enabled = true,
+		opts = {
+			ignored_next_char = "",
+			enable_check_bracket_line = true,
+			map_complete = true,
+			map_cr = true,
+			auto_select = true
+		}
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		enabled = true,
+		opts = {},
+	}
+}
