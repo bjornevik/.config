4472cf913553e5c2332edaf4bc71c0f9f2f6821e
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-09 08:47:14 +0100

    fix(nnv): lsp format on save

--- neoneovim/lua/plugins/lsp.lua
+++ neoneovim/lua/plugins/lsp.lua
@@ -18,7 +18,26 @@ return {
 		config = function()
 			require("lspconfig").lua_ls.setup {}
 
+			-- TODO: conform.nvim instead
 			vim.keymap.set("n", "<space>nf", function() vim.lsp.buf.format() end, { desc = "lsp format" })
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then return end
+
+					---@diagnostic disable-next-line: missing-parameter
+					if client.supports_method('textDocument/formatting') then
+						-- Format the current buffer on save
+						vim.api.nvim_create_autocmd('BufWritePre', {
+							buffer = args.buf,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+							end
+						})
+					end
+				end
+			})
 		end
 	}
 }
