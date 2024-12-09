e5329c8694004a31848e3101e32f667317fdc6f7
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-09 14:44:42 +0100

    feat(nnv): snacks.nvim

---
+++ neoneovim/lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/snacks.nvim",
+	enabled = true,
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = false },
+		notifier = { enabled = false },
+		quickfile = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false }
+	},
+	keys = {
+		{ "<leader>lg", function() Snacks.lazygit() end,        desc = "(l)azy(g)it" },
+		{ "<leader>.",  function() Snacks.scratch() end,        desc = "toggle scratch buffer" },
+		{ "<leader>gb", function() Snacks.git.blame_line() end, desc = "(g)it (b)lame line" },
+		{ "<leader>un", function() Snacks.notifier.hide() end,  desc = "dismiss all notifications" }
+	}
+}
