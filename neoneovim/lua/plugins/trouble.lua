97587c413f105c5981b47377ded19e50ef8104f4
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-09 13:08:48 +0100

    feat(nnv): basic trouble.nvim setup

---
+++ neoneovim/lua/plugins/trouble.lua
@@ -0,0 +1,34 @@
+return {
+	{
+		"folke/trouble.nvim",
+		opts = {
+			modes = {
+				diagnostics_buffer = {
+					mode = "diagnostics",
+					filter = { buf = 0 }
+				},
+				lsp_references = {
+					mode = "lsp_references",
+					follow = false,
+					auto_jump = false
+				},
+				lsp_document_symbols = {
+					desc = "document symbols",
+					mode = "lsp_document_symbols",
+					focus = true,
+					win = { position = "right" }
+				}
+			}
+		},
+		cmd = "Trouble",
+		keys = function()
+			local trouble = require("trouble")
+			return {
+				{ "<C-q>",     function() trouble.toggle "quickfix" end,                        noremap = true, silent = true,              desc = "Quickfix (trouble)" },
+				{ "<leader>q", function() trouble.toggle "diagnostics" end,                     noremap = true, silent = true,              desc = "Diagnostics (trouble)" },
+				{ "<C-j>",     function() trouble.next { skip_groups = true, jump = true } end, noremap = true, desc = "Next (trouble)" },
+				{ "<C-k>",     function() trouble.prev { skip_groups = true, jump = true } end, noremap = true, desc = "Previous (trouble)" }
+			}
+		end
+	}
+}
