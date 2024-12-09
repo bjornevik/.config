c606d327b485d1e3cad8a9a7eacdb942774cab26
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-09 09:32:26 +0100

    fix(nnv): use nvim-web-devicons for oil.nvim instead of mini.icons

--- neoneovim/lua/plugins/oil.lua
+++ neoneovim/lua/plugins/oil.lua
@@ -3,7 +3,7 @@ return {
 		"stevearc/oil.nvim",
 		enabled = true,
 		opts = {},
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("oil").setup({ columns = { "icon" }, view_options = { show_hidden = true } })
 			vim.keymap.set("n", "<leader>fb", "<cmd>Oil<cr>", { desc = "Open parent directory" })
