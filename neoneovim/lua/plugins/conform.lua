fea3148490d05ecc083c845e8070db2053083576
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-16 09:39:37 +0100

    nnv: conform.nvim

---
+++ neoneovim/lua/plugins/conform.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		lazy = false,
+		opts = {
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+				stop_after_first = true,
+			},
+			formatters_by_ft = {
+				astro = { "prettierd", "prettier" },
+				css = { "prettierd", "prettier" },
+				go = { "gofmt" },
+				html = { "prettierd", "prettier" },
+				javascript = { "prettierd", "prettier" },
+				json = { "jq" },
+				lua = { "stylua" },
+				markdown = { "prettierd", "prettier" },
+				ocaml = { "ocamlformat " },
+				rust = { "rustfmt" },
+				typescript = { "prettierd", "prettier" },
+				vue = { "prettierd", "prettier" },
+			},
+		},
+		keys = function()
+			local cf = require("conform")
+			return {
+				{ "<leader>nf", function()cf.format { async = true, lsp_format = "fallback", stop_after_first = true} end, desc = "conform: format" },
+			}
+		end,
+	},
+}
