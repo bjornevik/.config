b8647794b00fe63875a5da45c74b8300f8be887e
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-17 12:44:21 +0100

    nnv: more opts

--- neoneovim/lua/config/opts.lua
+++ neoneovim/lua/config/opts.lua
@@ -1,6 +1,18 @@
 -- sync OS and Neovim clipboards
 vim.opt.clipboard = "unnamedplus"
 
+-- This needs to be defined in an autocmd because ftplugins set their own formatoptions
+-- I don't want to redo this vim.opt in every `ftplugin/*.xx`, so overriding here.
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("custom_formatoptions", {}),
+	callback = function()
+		-- :help fo-table
+		-- c -> auto-wrap comments using 'textwidth'
+		-- o -> auto insert current comment leader after hitting 'o' or 'O'
+		vim.opt.formatoptions:remove { "c", "o" }
+	end,
+})
+
 vim.opt.signcolumn = "yes"
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -22,10 +34,15 @@ vim.opt.updatetime = 250
 vim.opt.splitright = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣", space = "·" }
+vim.opt.listchars = { tab = "» ", trail = "~", nbsp = "␣", space = "·" }
 
 vim.opt.inccommand = "split"
 
 vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
+
+vim.opt.swapfile = false -- Does snacks.bigfile fix this?
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath "config" .. "/undodir"
+vim.opt.undofile = true
