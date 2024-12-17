ef0c25bd94ca8db2a067eb04532f9cecdca4d813
John Even Bjørnevik <Johnbjornevik@gmail.com>
2024-12-17 09:19:56 +0100

    nnv: lsp method keymaps
    added keymaps for some of my most used lsp actions
    
    - "textDocument/hover"
    - "textDocument/definition"
    - "textDocument/inlayHint"
    - "textDocument/rename"
    - "textDocument/codeAction"
    - "textDocument/references"

--- neoneovim/lua/plugins/lsp.lua
+++ neoneovim/lua/plugins/lsp.lua
@@ -32,11 +32,42 @@ return {
 						return
 					end
 
+					if client.supports_method(client, "textDocument/hover") then
+						vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+					end
+
+					if client.supports_method(client, "textDocument/definition") then
+						vim.keymap.set(
+							"n",
+							"gd",
+							vim.lsp.buf.definition,
+							{ buffer = 0, desc = "LSP: Go to definition" }
+						)
+					end
+
 					if client.supports_method(client, "textDocument/inlayHint") then
 						vim.keymap.set("n", "<leader>uh", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 						end, { noremap = true, desc = "LSP inlay hints", buffer = 0 })
 					end
+
+					if client.supports_method(client, "textDocument/rename") then
+						vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = 0, desc = "LSP: rename" })
+					end
+
+					if client.supports_method(client, "textDocument/codeAction") then
+						vim.keymap.set(
+							"n",
+							"<leader>a",
+							vim.lsp.buf.code_action,
+							{ buffer = 0, desc = "LSP: code action" }
+						)
+					end
+
+					-- TODO: I never actually use these, do I need to add them?
+					-- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+					-- vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+					-- vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = 0 })
 				end,
 			})
 		end,
--- neoneovim/lua/plugins/trouble.lua
+++ neoneovim/lua/plugins/trouble.lua
@@ -45,6 +45,7 @@ return {
 				{
 					"<C-j>",
 					function()
+						---@diagnostic disable-next-line: missing-parameter, missing-fields
 						trouble.next { skip_groups = true, jump = true }
 					end,
 					noremap = true,
@@ -53,12 +54,37 @@ return {
 				{
 					"<C-k>",
 					function()
+						---@diagnostic disable-next-line: missing-parameter, missing-fields
 						trouble.prev { skip_groups = true, jump = true }
 					end,
 					noremap = true,
 					desc = "Previous (trouble)",
 				},
 			}
+		end,
+		init = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("trouble_on_attach", {}),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then
+						return
+					end
+					local trouble = require "trouble"
+
+					if client.supports_method(client, "textDocument/references") then
+						vim.keymap.set("n", "gr", function()
+							trouble.toggle "lsp_references"
+						end, { buffer = 0, desc = "LSP References" })
+					end
+
+					-- if client.supports_method(client, "textDocument/documentSymbol") then
+					-- 	vim.keymap.set("n", "<leader>ds", function()
+					-- 		require("trouble").toggle "lsp_document_symbols"
+					-- 	end, { buffer = 0 })
+					-- end
+				end,
+			})
 		end,
 	},
 }
