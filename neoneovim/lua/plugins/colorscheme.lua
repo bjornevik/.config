ef9829af3157f073681c72be3789c60461217f73
John Even Bjørnevik <Johnbjornevik@gmail.com>
2025-01-06 09:28:43 +0100

    nnv: gruvbox-material

--- neoneovim/lua/plugins/colorscheme.lua
+++ neoneovim/lua/plugins/colorscheme.lua
@@ -10,12 +10,23 @@ return {
 	},
 	{
 		"luisiacc/gruvbox-baby",
-		enabled = true,
+		enabled = false,
 		lazy = false,
 		priority = 1000,
 		config = function()
 			vim.g.gruvbox_baby_use_original_palette = true
 			vim.cmd.colorscheme "gruvbox-baby"
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_disabled_italic_comment = false
+			vim.g.gruvbox_material_foreground = "original"
+			vim.g.gruvbox_material_background = "soft"
+			vim.cmd.colorscheme "gruvbox-material"
 		end,
 	},
 }
