1058adc45b1036d53c29f6404a04ed5d4b0b68dd
John Even Bjørnevik <Johnbjornevik@gmail.com>
2025-01-08 09:09:21 +0100

    nnv: bufferline move cmds

--- neoneovim/lua/plugins/bufferline.lua
+++ neoneovim/lua/plugins/bufferline.lua
@@ -18,6 +18,22 @@ return {
 				silent = true,
 				desc = "bufferline: (g)o to (b)uffer",
 			},
+			{
+				"<M-h>",
+				":BufferLineMovePrev<CR>",
+				mode = { "n" },
+				noremap = true,
+				silent = true,
+				desc = "bufferline: move buffer to the left",
+			},
+			{
+				"<M-l>",
+				":BufferLineMoveNext<CR>",
+				mode = { "n" },
+				noremap = true,
+				silent = true,
+				desc = "bufferline: move buffer to the right",
+			},
 		},
 	},
 }
