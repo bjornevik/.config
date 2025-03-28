897f26da68db3b6cd2d395e25527ee364b12aaf6
John Even Bjørnevik <Johnbjornevik@gmail.com>
2025-03-28 11:55:18 +0100

    hammerspoon: apple (p)odcasts

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,6 +16,7 @@ hs.hotkey.bind(hyper, "e", open_app("Microsoft Outlook")) -- (e)mail
 hs.hotkey.bind(hyper, "f", open_app("Figma")) -- (f)igma
 hs.hotkey.bind(hyper, "i", open_app("Bruno")) -- formerly (i)nsomnia
 hs.hotkey.bind(hyper, "m", open_app("Music")) -- apple (m)usic
+hs.hotkey.bind(hyper, "p", open_app("Podcasts")) -- apple (p)odcasts
 hs.hotkey.bind(hyper, "o", open_app("Obsidian")) -- (o)bsidian
 hs.hotkey.bind(hyper, "p", open_app("Podcasts")) -- apple (p)odcas
 hs.hotkey.bind(hyper, "q", open_app("Simulator")) -- q cause unused
