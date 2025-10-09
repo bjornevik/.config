cc6f84bf7e89e863b460421c9a0e9879be070d73
John Even Bjørnevik <Johnbjornevik@gmail.com>
2025-10-09 08:34:44 +0200

    nvim(leap/flit): replace with 1 char enhanced search
    because flit is broken rn

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -4,14 +4,50 @@ return {
 		enabled = true,
 		dependencies = {
 			"tpope/vim-repeat",
-			{
-				"ggandor/flit.nvim",
-				opts = {},
-			},
+			-- {
+			-- 	"ggandor/flit.nvim",
+			-- 	opts = {},
+			-- },
 		},
 		keys = {
 			{ "s", "<Plug>(leap-forward)", mode = { "n", "x" }, noremap = false, desc = "leap forward" },
 			{ "gs", "<Plug>(leap-backward)", mode = { "n", "x" }, noremap = false, desc = "leap backward" },
 		},
+		config = function()
+			-- 1-character search (enhanced f/t motions)
+			-- ref: https://github.com/ggandor/leap.nvim/blob/f5fe479e20d809df7b54ad53142c2bdb0624c62a/README.md?plain=1#L695
+			do
+				-- Returns an argument table for `leap()`, tailored for f/t-motions.
+				local function as_ft(key_specific_args)
+					local common_args = {
+						inputlen = 1,
+						inclusive = true,
+						-- To limit search scope to the current line:
+						-- pattern = function (pat) return '\\%.l'..pat end,
+						opts = {
+							labels = "", -- force autojump
+							safe_labels = vim.fn.mode(1):match "o" and "" or nil, -- [1]
+							case_sensitive = true, -- [2]
+						},
+					}
+					return vim.tbl_deep_extend("keep", common_args, key_specific_args)
+				end
+
+				local clever = require("leap.user").with_traversal_keys -- [3]
+				local clever_f = clever("f", "F")
+				local clever_t = clever("t", "T")
+
+				for key, args in pairs {
+					f = { opts = clever_f },
+					F = { backward = true, opts = clever_f },
+					t = { offset = -1, opts = clever_t },
+					T = { backward = true, offset = 1, opts = clever_t },
+				} do
+					vim.keymap.set({ "n", "x", "o" }, key, function()
+						require("leap").leap(as_ft(args))
+					end)
+				end
+			end
+		end,
 	},
 }
