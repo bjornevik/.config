92d463477b9a325b19c4b548fe5687a443c8f07d
John Even Bjørnevik <Johnbjornevik@gmail.com>
2026-01-26 12:19:12 +0100

    nvim: vim.highlight is deprecated, use vim.hl
    for on_yank() :)

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,7 +25,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "highlight when yanking text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
 
