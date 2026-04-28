f97dcbbc4762831d711b66b272aaed1ea9574f47
John Even Bjørnevik <Johnbjornevik@gmail.com>
2026-04-28 13:51:13 +0200

    nvim: update nvim-treesitter to main branch
    cause I keep getting annoying `attempt to call method 'range' (a nil
    value)` error.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,76 +1,69 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
-		dependencies = {
-			{ "nvim-treesitter/nvim-treesitter-textobjects" },
-		},
-		config = function(a, b)
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup {
-				ensure_installed = {
-					"lua",
-					"go",
-					"typescript",
-					"tsx",
-					"javascript",
-					"vue",
-					"svelte",
-					"html",
-					"css",
-					"json",
-					"json5",
-					"toml",
-					"terraform",
-					"dockerfile",
-					"proto",
-					"ocaml",
-					"rust",
-					"vimdoc",
-					"gosum",
-					"gomod",
-					"markdown",
-				},
-				ignore_install = { "org" },
-				auto_install = false,
-				highlight = {
-					enable = true,
-					disable = function(lang, buf)
-						local max_filesize = 100 * 1024 -- 100 KB
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							return true
-						end
-					end,
-					additional_vim_regex_highlighting = false,
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["as"] = "@local.scope",
-							["ap"] = "@parameter.outer",
-							["ip"] = "@parameter.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@parameter.inner",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@parameter.inner",
-						},
-					},
-				},
+		config = function()
+			local ts = require "nvim-treesitter"
+			ts.setup()
+
+			local ensure_installed = {
+				"lua",
+				"go",
+				"typescript",
+				"tsx",
+				"javascript",
+				"vue",
+				"svelte",
+				"html",
+				"css",
+				"json",
+				"json5",
+				"toml",
+				"terraform",
+				"dockerfile",
+				"proto",
+				"ocaml",
+				"rust",
+				"vimdoc",
+				"gosum",
+				"gomod",
+				"markdown",
+			}
+
+			pcall(ts.install, ensure_installed)
+
+			local group = vim.api.nvim_create_augroup("UserTreesitterMain", {
+				clear = true,
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				callback = function(ev)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(ev.buf))
+					if ok and stats and stats.size > max_filesize then
+						return
+					end
+
+					pcall(vim.treesitter.start, ev.buf)
+					vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		init = function()
+			vim.g.no_plugin_maps = true
+		end,
+		config = function()
+			require("nvim-treesitter-textobjects").setup {
+				select = { lookahead = true },
+				move = { set_jumps = true },
 			}
 		end,
 	},
